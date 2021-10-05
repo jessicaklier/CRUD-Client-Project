5d400e80768daba04e535fb8bd33043524b6003d
jessica klier <jhessicadesouza12@hotmail.com>
2021-10-05 15:01:36 -0300

    Post Clients

--- backend/src/main/java/com/devsuperior/client/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/client/services/ClientService.java
@@ -33,4 +33,17 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new EntityNotFoundException ("Entidade não encontrada"));
 		return new ClientDTO(entity);
 	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+		
+	}
 }
