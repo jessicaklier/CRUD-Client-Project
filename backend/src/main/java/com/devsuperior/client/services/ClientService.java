2c27b5d675ea6485bfb6eff3356631203a610f0c
jessica klier <jhessicadesouza12@hotmail.com>
2021-10-05 16:20:10 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/client/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/client/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.client.dto.ClientDTO;
 import com.devsuperior.client.entities.Client;
 import com.devsuperior.client.repositories.ClientRepository;
+import com.devsuperior.client.services.exceptions.DatabaseException;
 import com.devsuperior.client.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -63,6 +66,18 @@ public class ClientService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria Silva', '123.450.789-01','6.500', '1994-07-20T10:30:00Z','2');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jessica Klier', '121.241.041-01','9.500', '1994-12-01T10:30:00Z','0');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jean Ferraz', '122.242.042-02','7.500', '1988-11-28T10:30:00Z','0');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jeferson Marcilio', '123.243.043-03','3.500', '1997-12-14T10:30:00Z','2');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Juliano Eduardo', '124.244.044-04','7.500', '2000-10-17T10:30:00Z','0');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Terezinha Paz', '125.245.045-05','9.500', '1976-08-29T10:30:00Z','3');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcilio Carlos', '126.246.046-06','9.500', '1972-05-08T10:30:00Z','3');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jose Pacheco', '127.247.047-07','3.500', '1966-01-20T10:30:00Z','6');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Air Carlos', '128.248.049-08','2.500', '1976-07-02T10:30:00Z','0');
-INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joao Marcos', '129.249.040-01','1.500', '1990-09-08T10:30:00Z','1');
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Maria Silva', '123.450.789-01','6.500', '1994-07-20T10:30:00Z','2', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Jessica Klier', '121.241.041-01','9.500', '1994-12-01T10:30:00Z','0', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Jean Ferraz', '122.242.042-02','7.500', '1988-11-28T10:30:00Z','0', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Jeferson Marcilio', '123.243.043-03','3.500', '1997-12-14T10:30:00Z','2', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Juliano Eduardo', '124.244.044-04','7.500', '2000-10-17T10:30:00Z','0', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Terezinha Paz', '125.245.045-05','9.500', '1976-08-29T10:30:00Z','3', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Marcilio Carlos', '126.246.046-06','9.500', '1972-05-08T10:30:00Z','3', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Jose Pacheco', '127.247.047-07','3.500', '1966-01-20T10:30:00Z','6', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Air Carlos', '128.248.049-08','2.500', '1976-07-02T10:30:00Z','0', NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ('Joao Marcos', '129.249.040-01','1.500', '1990-09-08T10:30:00Z','1', NOW());
 
 
